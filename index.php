3763198712b22fd300d8d3424ed1b8620c5c4366
SaraCS21 <saracs15.scssn@gmail.com>
2023-01-30 22:18:05 +0000

    cambio procfile

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!-- Llamamos al autoload -->
-<?php require __DIR__ . "./vendor/autoload.php"; ?> 
+<!-- <?php require __DIR__ . "./vendor/autoload.php"; ?>  -->
+<?php require "./vendor/autoload.php"; ?> 
 
 <!-- Llamada al header de la web -->
 <?php require "./parts/header.php" ?>
